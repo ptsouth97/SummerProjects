b18debd1aec9afc5992ea3611bf469d380f7f1ff
ocros03 <ocros03@gmail.com>
2017-06-04 16:13:09 -0400

    updated daily folders

---
+++ Week1/Day1/README.md
@@ -0,0 +1,11 @@
+#Instructions for Day 1
+
+I. Update your project files with git
+	a) navigate to /Documents/OrionSummerProject
+	b) type: 'git pull origin master'  (without the quotes)
+
+II. Read what you need to do today
+	a) Read the README.md in the top level of the project
+		i) You can use 'cat README.md' or 'vim README.md'
+	b) Navigate to /Documents/OrionSummerProject/Week1/Day1
+	c) Read the Day1 README.md using the same method
---
+++ Week1/Day4/Pygame3-Animation.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3.6
+
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()  													# activates Pygame, always the first command after import statements
+
+FPS = 30														# frames per second setting
+fpsClock = pygame.time.Clock()
+
+# set up the window
+DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)		# creates a surface object of size width x height
+pygame.display.set_caption('Animation')							# sets the text at the top of the window
+
+WHITE = (255, 255, 255)
+catImg = pygame.image.load('cat.png')
+catx = 10
+caty = 10
+direction = 'right'
+
+while True:  													# main game loop: 1) handles events, 2) updates game state, 3) draws game state to screen
+	DISPLAYSURF.fill(WHITE)
+
+	if direction == 'right':
+		catx += 5
+		if catx == 280:
+			direction = 'down'
+	elif direction == 'down':
+		caty += 5
+		if caty == 220:
+			direction = 'left'
+	elif direction == 'left':
+		catx -= 5
+		if catx == 10:
+			direction = 'up'
+	elif direction == 'up':
+		caty -= 5
+		if caty == 10:
+			direction = 'right'
+
+	DISPLAYSURF.blit(catImg, (catx, caty))						# blit: draw the contents of one surface onto another...e.g. copy the cat image to surface object
+
+	for event in pygame.event.get():							# event handling: checks to see if certain events have occurred
+		if event.type == QUIT:
+			pygame.mixer.music.stop()
+			pygame.quit()										# deactivates Pygame...opposite of pygame.init()
+			sys.exit()											# terminates program
+	pygame.display.update()										# draws the surface object held by the DISPLAYSURF variable
+	fpsClock.tick(FPS)
